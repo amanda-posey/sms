a1e490fe104f7b0cb9974fec9b456ca9ac45d8cf
amanda=posey <amanda.posey@live.com>
2022-03-12 02:01:55 -0500

    working on navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import logo from '../images/sms_logo_long.png';
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return(
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a href="https://bulma.io">
+      <img src={logo} width="500" height="250"/>
+    </a>
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item">
+        Home
+      </a>
+
+      <a class="navbar-item">
+        Services
+      </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+          More
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            About
+          </a>
+          <a class="navbar-item">
+            Jobs
+          </a>
+          <a class="navbar-item">
+            Contact
+          </a>
+          <hr class="navbar-divider"/>
+          <a class="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+  )
+}
+
+export default Navbar
---
+++ src/components/Services.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <section class="section">
+        <div class="container">
+          <h1 class="title">
+            Services
+          </h1>
+          <p class="subtitle">
+            Articles containing info about services rendered. Maybe include:
+            <ul>
+              <li>Price list</li>
+              <li>Info about boarding</li>
+              <li>Info about shows</li>
+              <li>Levels of riding instruction</li>
+            </ul>
+          </p>
+        </div>
+      </section>
+    );
+}
+
+export default Services;
